0d1790de4a2ace387886639f401afe3df84895f8
nguyenkaos <nguyen.caal@gmail.com>
2017-09-16 15:56:44 +0700

    ok

---
+++ new7.py
@@ -0,0 +1,190 @@
+
+###_________________________AWS____________________________###
+
+##compte 1 :
+caoazerty04@gmail.com:Chel_sea111
+ubuntu@ec2-18-221-138-229.us-east-2.compute.amazonaws.com
+
+
+
+
+
+
+
+
+############# create new #############
+sudo apt-get update  && sudo apt-get install -y tmux python-pip vim && pip install requests && git clone https://github.com/daipaw0001/mmm && cd mmm && tmux new-session -d "sudo dpkg -i minergate-cli.deb && minergate-cli -user caoazerty01@gmail.com -xmr 2"
+
+
+## tmux
+
+tmux new-session -d "python python2.py"
+tmux kill-session -t 0
+tmux new-session -d "sudo dpkg -i minergate-cli.deb && minergate-cli -user caoazerty01@gmail.com -xmr 1 && minergate-cli -user caoazerty01@gmail.com -xmr 1"
+
+############# re-connect #############
+sudo apt-get update  && sudo apt-get install -y tmux python-pip vim && pip install requests 
+
+
+sudo dpkg -i minergate-cli.deb 
+sudo minergate-cli -user caoazerty01@gmail.com -xmr 2 
+sudo minergate-cli -user caoazerty01@gmail.com -xmr 1
+
+
+
+## tmux 0
+sudo dpkg -i minergate-cli.deb
+sudo minergate-cli -user caoazerty01@gmail.com -xmr 2
+
+## tmux 1
+ 
+-------------------------
+ 
+irvr@psles.com
+
+----------------------
+
+import requests, time
+ 
+cook2 = 'JSESSIONID=F96E7350EB938FD595B02205795B788C; session-access-key=eyHWqLO8KLjj49zjzO4PCHe8Hn5TP9i4Pu0ivzn9unyWavj4bKPPKWnXbCCfeiqCSuX8bCKOe5P8W9LurzSG0rrjW4vbmHqjGjaaOfevLD9vDvLHPDjybP9n50SjDrq5SruSPPqvn0X9WKLvKbLXWCGCTjSuHXuTuWmuq5jSyT0qm1HTOKXunTrujO114HvH5ryG1P4P9XmO15i0Sy9WvuTD8CbDuTOOrXKX0rjCum5qaSHHOSf50K9anjS5j1D; logged_in=true; __utma=236179731.605771095.1504464267.1504464267.1504464267.1; __utmb=236179731.4.10.1504464267; __utmc=236179731; __utmz=236179731.1504464267.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmt_exo=1; _ga=GA1.2.605771095.1504464267; _gid=GA1.2.1661123964.1504464274; IDESESSIONID=3F94A69F1FB6BBC259B59725EB2B9791'
+
+crsf = "FC777F7C766022D46823FE46925F66A4"
+
+refer = 'https://codenvy.io/vkxr/wksp-l5md'
+
+auth = 'machinery20xavyioyq9k9ki24p2y7kp2gfjuzg62c2yiggb47q8iqi4ci2aipxqu1m4p4em2jgdu2mzwxl6t168d7t27kgyvanee0nl24525n88s1ivyg6p2cleqkvbjsm032j'
+ 
+url2= 'https://codenvy.io/api/activity/workspace4pb0e4mnh4b7xqee'
+ 
+payload2 = {
+    "Host": "codenvy.io",
+    "Connection": "keep-alive",
+    "Content-Length": "0",
+    "Origin": "https://codenvy.io",
+    "X-Requested-With": "XMLHttpRequest", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5",
+    "Content-Type": "application/json",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip,deflate,br",
+    "Accept-Language": "en-US,en;q=0.8", 
+    "X-CSRF-Token": crsf, 
+    "Authorization":auth,
+    "Referer": refer,
+    'Cookie':cook2
+}
+
+
+while 1: 
+    rr2= requests.put(url2,headers=payload2,data='print "'+str(time.ctime())+'"')
+    rr3= requests.get(url2,headers=payload2,data='print "'+str(time.ctime())+'"') 
+    print rr2.status_code 
+    print rr3.status_code 
+    print '----- ',":", str(time.ctime()),' -----'
+    time.sleep(15)
+
+
+
+    
+    
+_________________________________
+_________________________________
+_________________________________
+_________________________________
+_________________________________
+_________________________________
+
+
+
+
+
+
+acc =  "eour@i.xcode.ro"
+
+import requests, time
+
+cook1= 'JSESSIONID=47F4D7567A8B37C1B0E2E243BA13CB72; __ar_v4=2TZBGUFB4NG3XOXIT65JNC%3A20170819%3A10%7CZG5Z2TEJUZBPBFFAJOVBWT%3A20170819%3A10%7CBZOLVVOXJVEOFDOIOAFMVC%3A20170819%3A10; __utmt=1; __utmt_exo=1; __utma=236179731.1473880644.1503166301.1504027677.1504038964.22; __utmb=236179731.2.10.1504038964; __utmc=236179731; __utmz=236179731.1503243268.3.2.utmcsr=github.com|utmccn=(referral)|utmcmd=referral|utmcct=/codenvy/docs/; session-access-key=yDDX4HTSfH98jTPbP1aGL4m0reb4yOjLayarr1a9n4COH9HTaO8ybeGW9WOHzHDn5GKbi0POjjrrSeXrHuLi91D4PLn9uz1iGWCvDayrS0TXT99CL9K0eCqeuui8ajf0WqSb8TCCHC49XWXaGG0PTGySLGXa8iCG8aee1LaO1fPeubjqfCaiHTSeXP4biaSK94CTrSOvG1PiG0Obbvyi5v0u8H5u8faHyeCP88TGHfrOOHauGG4GCTznyzbqK9a; logged_in=true; IDESESSIONID=80752EAA115ACEC4E57D70E44B122EFF; _ga=GA1.2.1473880644.1503166301; _gid=GA1.2.1777308398.1503760795'
+
+cook2 = 'JSESSIONID=6A628A32B44F608AB22A279292A2A63C; __ar_v4=2TZBGUFB4NG3XOXIT65JNC%3A20170819%3A10%7CZG5Z2TEJUZBPBFFAJOVBWT%3A20170819%3A10%7CBZOLVVOXJVEOFDOIOAFMVC%3A20170819%3A10; __utmt=1; __utmt_exo=1; __utma=236179731.1473880644.1503166301.1504027677.1504038964.22; __utmb=236179731.2.10.1504038964; __utmc=236179731; __utmz=236179731.1503243268.3.2.utmcsr=github.com|utmccn=(referral)|utmcmd=referral|utmcct=/codenvy/docs/; session-access-key=yDDX4HTSfH98jTPbP1aGL4m0reb4yOjLayarr1a9n4COH9HTaO8ybeGW9WOHzHDn5GKbi0POjjrrSeXrHuLi91D4PLn9uz1iGWCvDayrS0TXT99CL9K0eCqeuui8ajf0WqSb8TCCHC49XWXaGG0PTGySLGXa8iCG8aee1LaO1fPeubjqfCaiHTSeXP4biaSK94CTrSOvG1PiG0Obbvyi5v0u8H5u8faHyeCP88TGHfrOOHauGG4GCTznyzbqK9a; logged_in=true; IDESESSIONID=80752EAA115ACEC4E57D70E44B122EFF; _ga=GA1.2.1473880644.1503166301; _gid=GA1.2.1777308398.1503760795'
+
+crsf = "70709EE65AF3EBE8E8EDEC080725E7CB"
+
+refer = 'https://codenvy.io/fze/wksp-tbeq'
+
+auth = 'machinenlss2ndy6rslxsd2wda8r81shemuwb9kczn0ek9e3s43hrohirx2pllhcto5efmv9hdfs3fpvk65lsdmx5an97oybwd2dw9fzz960ff0mwr5fa0jsayctt7gvoggj7kn'
+
+url1 = 'https://codenvy.io/api/activity/workspacez19jsth3ddkvw1js'
+
+url2= 'https://codenvy.io/57274_node26.codenvy.io/api/project/file/mmm/zz.py'
+
+payload1 = {
+    "Host": "codenvy.io",
+    "Connection": "keep-alive",
+    "Content-Length": "0",
+    "Origin": "https://codenvy.io",
+    "X-Requested-With": "XMLHttpRequest", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5",
+    "Content-Type": "application/json",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip,deflate,br",
+    "Accept-Language": "en-US,en;q=0.8", 
+    "X-CSRF-Token": crsf, 
+    "Authorization":auth,
+    "Referer": refer,
+    'Cookie':cook1
+}
+
+
+payload2 = {
+    "Host": "codenvy.io",
+    "Connection": "keep-alive",
+    "Content-Length": "0",
+    "Origin": "https://codenvy.io",
+    "X-Requested-With": "XMLHttpRequest", 
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5",
+    "Content-Type": "application/json",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip,deflate,br",
+    "Accept-Language": "en-US,en;q=0.8", 
+    "X-CSRF-Token": crsf, 
+    "Authorization":auth,
+    "Referer": refer,
+    'Cookie':cook2
+}
+
+
+while 1:
+    # rr1= requests.put(url1,headers=payload1)
+    rr2= requests.put(url2,headers=payload2,data='print "'+str(time.ctime())+'"')
+    rr3= requests.get(url2,headers=payload2,data='print "'+str(time.ctime())+'"')
+    # print rr1.status_code
+    print rr2.status_code 
+    print rr3.status_code 
+    print '----- ',acc,":", str(time.ctime()),' -----'
+    time.sleep(20)
+
+
+
+
+  
+  
+  
+### 
+sudo rm /var/cache/apt/archives/lock  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
